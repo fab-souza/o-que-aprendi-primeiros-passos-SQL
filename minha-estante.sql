d2db3214cdf7de3d2e4e8a993ea9433d53123420
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-04-19 15:02:07 -0300

    Update minha-estante.sql

--- minha-estante.sql
+++ minha-estante.sql
@@ -3,5 +3,20 @@ CREATE TABLE estante (
   titulo VARCHAR(255),
   autor VARCHAR(255),
   idioma_pt_br BOOLEAN,
-  nota NUMERIC,
+  nota NUMERIC
 );
+
+
+INSERT INTO estante (id, titulo, autor, idioma_pt_br, nota) 
+	VALUES (1,'O Nome do Vento', 'Patric Rothfuss', TRUE, 5)
+	
+SELECT * FROM estante
+	
+INSERT INTO estante (id, titulo, autor, idioma_pt_br, nota) 
+	VALUES (2,'Recursão', 'Blake Crouch', TRUE, 5)
+INSERT INTO estante (id, titulo, autor, idioma_pt_br, nota) 
+	VALUES (3,'Tigers Curse', 'Colleen Houck', FALSE, 4)
+INSERT INTO estante (id, titulo, autor, idioma_pt_br, nota) 
+	VALUES (4,'Trigun', 'Yasuhiro Nightow', FALSE, 5)
+	
+SELECT * FROM estante	
