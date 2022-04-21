fe0d384a66b0f22409804ff080a0d35fd3ee5834
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-04-21 13:33:52 -0300

    Update minha-estante.sql
    Criando outra tabela

--- minha-estante.sql
+++ minha-estante.sql
@@ -20,3 +20,25 @@ INSERT INTO estante (id, titulo, autor, idioma_pt_br, nota)
 	VALUES (4,'Trigun', 'Yasuhiro Nightow', FALSE, 5)
 	
 SELECT * FROM estante	
+
+CREATE TABLE goodreads (
+	id SERIAL PRIMARY KEY,
+	score NUMERIC NOT NULL
+);
+
+DROP TABLE goodreads
+
+CREATE TABLE goodreads (
+	id SERIAL PRIMARY KEY,
+	score NUMERIC NOT NULL,
+	ratings NUMERIC NOT NULL,
+	reviews NUMERIC NOT NULL
+);
+
+INSERT INTO goodreads (score, ratings, reviews) VALUES (4.52, 825335, 47418);
+
+SELECT * FROM goodreads;
+
+INSERT INTO goodreads (score, ratings, reviews) VALUES (4.15, 163132, 20820);
+INSERT INTO goodreads (score, ratings, reviews) VALUES (4.06, 60405, 6053);
+INSERT INTO goodreads (score, ratings, reviews) VALUES (4.1, 197, 18);
